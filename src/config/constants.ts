064e04d0b881c7579eafc4ac4ce4890fbffa5478
jstinhw <justin84602@gmail.com>
2024-12-17 12:32:14 +0800

    fix: export intent address

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,9 @@
+import type { Address } from "viem";
+
 export const ZERODEV_URLS = {
   INTENT_SERVICE: "https://user-intent-service.onrender.com/intent",
   RELAYER_SERVICE: "https://relayer-d6ne.onrender.com",
 } as const;
+
+export const INTENT_EXECUTOR: Address =
+  "0x04Eb0aDE11ec34cd4F41f9Ed516ada5c2eBffad2";
