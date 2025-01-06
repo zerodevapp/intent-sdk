912679a9eee54d0094fbff7b7dcb7f1d9ad273ef
jhw <justin@moonchute.xyz>
2025-01-06 16:07:53 +0800

    fix: intentHash with SAME_CHAIN (#8)

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-import type { Address } from "viem";
+import type { Address, Hex } from "viem";
 
 export const ZERODEV_URLS = {
   INTENT_SERVICE: "https://user-intent-service.onrender.com/intent",
@@ -8,3 +8,7 @@ export const ZERODEV_URLS = {
 
 export const INTENT_EXECUTOR: Address =
   "0x3E1bF561DEbE46C22cc1e85EC8283A5EB49f5dae";
+
+// keccak256(abi.encode("SAME_CHAIN"));
+export const SAME_CHAIN_ORDER_DATA_TYPE: Hex =
+  "0xcb571c492be65e1ab6b3b279b36ac8d22472959a6c27d22460477e26bd32776a";
